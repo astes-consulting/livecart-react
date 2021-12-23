0ee6a8c7a6be562d4b0a3fc0fadcc64132e3e856
Filip ZBook <philip.ngimbis@gmail.com>
2021-12-23 19:16:42 +0100

    Implémentation de auth Firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Reset from "./pages/auth/Reset";
+import Home from "./pages/Home";
+import Secret from "./pages/protected/Secret";
+
+import { firebaseConfig } from "./configs/firebaseConfig";
+import { initializeApp } from "firebase/app";
+import { getAuth, signOut, onAuthStateChanged } from "firebase/auth";
+import { useSelector, useDispatch } from "react-redux";
+import { saveUser } from "./redux/slice/authSlice";
+import ProtectedRoute from "./utils/ProtectedRoute";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    initializeApp(firebaseConfig);
+
+    const auth = getAuth();
+    const user = useSelector((state) => state.auth.value);
+    console.log("user from state", user);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                dispatch(saveUser(user.refreshToken));
+            } else {
+                dispatch(saveUser(undefined));
+            }
+        });
+    }, [auth, dispatch]);
+
+    return (
+        <Router>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                    <li>
+                        <Link to="/register">Register</Link>
+                    </li>
+                    <li>
+                        <Link to="/reset">Reset password</Link>
+                    </li>
+                    <li>
+                        <Link to="/protected">Protected page</Link>
+                    </li>
+                    <li>
+                        <Link
+                            to="#"
+                            onClick={() => {
+                                signOut(auth)
+                                    .then(() => {
+                                        console.log("user signed out");
+                                    })
+                                    .catch((error) => {
+                                        console.log("error", error);
+                                    });
+                            }}
+                        >
+                            Log out
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <Routes>
+                <Route exact path="/register" element={<Register />} />
+                <Route exact path="/login" element={<Login />} />
+                <Route exact path="/reset" element={<Reset />} />
+
+                <Route exact path="/" element={<Home />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/configs/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyATzJvRHFp3GbMsuB6-J9Ukzc9s30Hod9s",
+    authDomain: "livechat-a6e59.firebaseapp.com",
+    databaseURL: "https://livechat-a6e59-default-rtdb.firebaseio.com",
+    projectId: "livechat-a6e59",
+    storageBucket: "livechat-a6e59.appspot.com",
+    messagingSenderId: "591019817410",
+    appId: "1:591019817410:web:6be6e5fa71bd5c933e7e5b",
+    measurementId: "G-GYBSCVBG45"
+};
